1ef6f68ae25aa8631cd867963dd2f33807e801c1
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-07-29 19:51:24 +0200

    Player Attributes - working

--- Main.py
+++ Main.py
@@ -3,9 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 from Team.PlayersFromTeam import findPlayersFromTeam
 from Team.TeamsFromLeague import findTeamsFromLeague
+from Player.AttributesFromPlayer import findPlayerAttributes
+import time
 import pandas as pd
 
-ekstraklasa = "https://www.transfermarkt.pl/jumplist/startseite/wettbewerb/PL1"
+ekstraklasa = "https://www.transfermarkt.com/jumplist/startseite/wettbewerb/PL1"
 firstLeague = "https://www.transfermarkt.pl/jumplist/startseite/wettbewerb/PL2"
 secondLeague = "https://www.transfermarkt.pl/jumplist/startseite/wettbewerb/PL2L"
 
@@ -15,15 +17,27 @@ playerIds = []
 playerNames = []
 playerTeams = []
 playerHyperlinks = []
+playerPositions = []
+playerAgents = []
+playerDatesOfBirth = []
 
 for i in range(0,len(teamHyperlinks)):
 	tempIds, tempNames, tempHyperlinks = findPlayersFromTeam(teamHyperlinks[i])
 	tempTeamName = teamNames[i]
 	for j in range(0, (len(tempIds))):
 		playerTeams.append(tempTeamName)
+		time.sleep(1)
+		tempDateOfBirth, tempPosition, tempAgent = findPlayerAttributes(tempHyperlinks[j])
+		playerDatesOfBirth.append(tempDateOfBirth)
+		playerPositions.append(tempPosition)
+		playerAgents.append(tempAgent)
+		print(tempNames[j] + " imported!")
 	playerIds.extend(tempIds)
 	playerNames.extend(tempNames)
 	playerHyperlinks.extend(tempHyperlinks)
+	
 
-df = pd.DataFrame({"ID":playerIds,"NAME":playerNames, "TEAM":playerTeams,"HYPERLINK":playerHyperlinks})
-df.to_excel("test.xlsx")
+# df = pd.DataFrame({"ID":playerIds,"NAME":playerNames, "TEAM":playerTeams,"HYPERLINK":playerHyperlinks})
+# df.to_excel("test.xlsx")
+
+print(playerPositions)
---
+++ Match/EventsFromMatch.py
@@ -0,0 +1,67 @@
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+#For pretending being a browser
+headers = {'User-Agent': 
+			   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
+page = "https://www.transfermarkt.com/spielbericht/index/spielbericht/3192599"
+page = "https://www.transfermarkt.com/spielbericht/index/spielbericht/3192601"
+
+
+
+#Getting full page
+pageTree = requests.get(page, headers=headers)
+pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+
+startingLineUpIdsTags = pageSoup.findAll("span", {"class" : ["aufstellung-rueckennummer-name","spielprofil_tooltip", "tooltipstered"]})
+goalTableTags = pageSoup.find("div", {"id" : "sb-tore"})
+substitutionsTableTags = pageSoup.find("div", {"id":"sb-wechsel"})
+
+goalPlayersTags = goalTableTags.findAll("a", {"class" : "wichtig"})
+outPlayerSpanTags = substitutionsTableTags.findAll("span", {"class" : "sb-aktion-wechsel-aus"})
+inPlayerSpanTags = substitutionsTableTags.findAll("span", {"class" : "sb-aktion-wechsel-ein"})
+
+outPlayerTags = []
+inPlayerTags = []
+
+
+startingLineUpIds = []
+goalsIds = []
+assistsIds = []
+outIds = []
+inIds = []
+
+for IdTag in startingLineUpIdsTags:
+	startingLineUpIds.append(IdTag["id"])
+
+for i in range(0, len(goalPlayersTags)):
+	#if even number
+	if (i % 2 == 0):
+		goalsIds.append((goalPlayersTags[i])['id'])
+	#if odd number	
+	else:	
+		assistsIds.append((goalPlayersTags[i])['id'])
+
+for outPlayerSpanTag in outPlayerSpanTags:
+	outPlayerTags.extend(outPlayerSpanTag.findAll("a", {"class" : "wichtig"}))
+
+for inPlayerSpanTag in inPlayerSpanTags:
+	inPlayerTags.extend(inPlayerSpanTag.findAll("a", {"class" : "wichtig"}))
+
+for i in range(0,len(outPlayerTags)):
+	outIds.append((outPlayerTags[i])['id'])
+
+for i in range(0,len(inPlayerTags)):
+	inIds.append((inPlayerTags[i])['id'])
+
+print (startingLineUpIds)
+print (goalsIds)
+print (assistsIds)
+print (outIds)
+print (inIds)
+
+
+
+
