1778fb45dc11f24254499517677bb630bdfe36a3
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-08-07 16:49:45 +0200

    Alias update

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
-echo "alias s4s='python3 desktop/GitLab/Transfermarkt/Transfermarkt/GUI.py'" >> ~/.bash_profile
-echo "alias S4S='python3 desktop/GitLab/Transfermarkt/Transfermarkt/GUI.py'" >> ~/.bash_profile
+echo "alias s4s='python3 desktop/GitLab/Transfermarkt/GUI.py'" >> ~/.bash_profile
+echo "alias S4S='python3 desktop/GitLab/Transfermarkt/GUI.py'" >> ~/.bash_profile
