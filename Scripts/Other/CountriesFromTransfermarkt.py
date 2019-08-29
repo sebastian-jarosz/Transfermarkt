6ccf764e9310dfd3c1bdb9569aaee338fd9fa018
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-08-29 17:07:00 +0200

    Countries from Transfermarkt

--- Scripts/Other/CountriesFromTransfermarkt.py
+++ Scripts/Other/CountriesFromTransfermarkt.py
@@ -3,24 +3,41 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import codecs
+import json
 
 
- #For pretending being a browser
-headers = {'User-Agent': 
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+def getCountriesFromTransfermarkt():
+    #For pretending being a browser
+    headers = {'User-Agent': 
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
-#1580 line
-f=codecs.open("transfermarkt.html", 'r')
-    
-#Getting full page
-# pageTree = requests.get(f.read(), headers=headers)
-pageSoup = BeautifulSoup(f, 'html.parser')
+    #1580 line
+    htmlpath = os.path.realpath(__file__)
+    htmlpath = htmlpath.rsplit('/', 1)[0] + "/transfermarkt.html"
 
-countryNames = []
+    f=codecs.open(htmlpath, 'r', encoding='utf-8', errors=' ignore')
 
-countriesList = pageSoup.find("select", {"data-placeholder" : "Country"}).find_all('option')
+    countries = []
 
-print(countriesList)
+    #Getting full page
+    # pageTree = requests.get(f.read(), headers=headers)
+    pageSoup = BeautifulSoup(f, 'html.parser')
 
-for country in countriesList:
-    print(country)
+    countryNames = []
+
+
+    countriesList = pageSoup.find("select", {"data-placeholder" : "Country"}).find_all('option')
+
+    del countriesList[0] #delete empty record
+
+    for country in countriesList:
+        tempCountry = {}
+        tempCountry['id'] = country['value']
+        tempCountry['name'] = country.text
+        tempCountry['hyperlink'] = 'https://www.transfermarkt.com/wettbewerbe/national/wettbewerbe/' + country['value']
+        countries.append(tempCountry)
+
+    for country in countries:
+        country['test'] = 'dupsko'
+
+    return countries
--- Scripts/Other/test.py
+++ Scripts/Other/test.py
@@ -2,9 +2,7 @@ import os
 import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
+from Scripts.Other.CountriesFromTransfermarkt import getCountriesFromTransfermarkt
 
 
-#Get folder path
-scriptpath = os.path.realpath(__file__)
-scriptpath = scriptpath.rsplit('/', 1)[0]
-print("Script path is : " + scriptpath)
+print(getCountriesFromTransfermarkt())
