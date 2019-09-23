145025ebb862507a079b782e5148d8367f60013b
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-09-23 22:03:35 +0200

    Test pools

--- test.py
+++ test.py
@@ -4,9 +4,25 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import codecs
 import json
+from multiprocessing import Pool
 from Scripts.Other.CountriesFromTransfermarkt import *
 
-getCountriesFromTransfermarkt()
-     
+COUNTRIES = []
+countriesJSON = getCountriesFromFile()
+for country in countriesJSON.keys():
+    COUNTRIES.append(country)
+
+hyperlinks = []
+for country in COUNTRIES:    
+    hyperlinks.append(countriesJSON[country]['hyperlink'])
+    print(country)
+
+p = Pool(10)
+records = p.map(getLeaguesFromCountry, hyperlinks)
+p.terminate()
+p.join()
+
+for record in records:
+    print(record)
 
 
