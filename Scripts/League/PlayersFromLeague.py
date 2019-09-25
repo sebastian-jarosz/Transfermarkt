893415903bd377328f59e2c2d823f103795fccde
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-09-25 18:24:57 +0200

    Pools - beginning

--- Scripts/League/PlayersFromLeague.py
+++ Scripts/League/PlayersFromLeague.py
@@ -58,3 +58,48 @@ def generateListOfPlayersFromLeague(countryName, leagueName, saison, LeagueHyper
 	
 	sg.Popup("End of export")
 
+
+def generateListOfPlayersFromLeaguePool(countryName, leagueName, saison, LeagueHyperlink):
+	sg.Popup("Start of export")
+	teamIds, teamNames, teamHyperlinks = findTeamsFromLeague(LeagueHyperlink)
+
+	playerIds = []
+	playerNames = []
+	playerTeams = []
+	playerHyperlinks = []
+	playerPositions = []
+	playerFoots = []
+	playerAgents = []
+	playerDatesOfBirth = []
+
+	if platform == "darwin":
+		directory = os.environ['HOME'] + "/Desktop/Transfermarkt Export/" + countryName + "/"+ str(saison) + "/" + leagueName
+		path = directory + "/Players.xlsx"
+	if platform == "win32":
+		directory = os.environ['HOMEPATH'] + "\Desktop\Transfermarkt Export\\" + countryName + "\\" + str(saison) + "\\" + leagueName
+		path = directory + "\Players.xlsx"
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	for i in range(0,len(teamHyperlinks)):
+		tempIds, tempNames, tempHyperlinks = findPlayersFromTeam(teamHyperlinks[i])
+		sg.OneLineProgressMeter('Export', i, len(teamHyperlinks), 'key','Export of players from teams')
+		tempTeamName = teamNames[i]
+		print("Start of import for " + teamNames[i])
+		for j in range(0, (len(tempIds))):
+			playerTeams.append(tempTeamName)
+			time.sleep(1.5)
+			tempDateOfBirth, tempPosition, tempAgent, tempFoot = findPlayerAttributes(tempHyperlinks[j])
+			playerDatesOfBirth.append(tempDateOfBirth)
+			playerPositions.append(tempPosition)
+			playerFoots.append(tempFoot)
+			playerAgents.append(tempAgent)
+		print("End of import for " + teamNames[i])
+		playerIds.extend(tempIds)
+		playerNames.extend(tempNames)
+		playerHyperlinks.extend(tempHyperlinks)
+	
+	sg.OneLineProgressMeter('Export',  len(teamHyperlinks), len(teamHyperlinks), 'key','Export of players from teams')
+	df = pd.DataFrame({"ID":playerIds,"NAME":playerNames, "TEAM":playerTeams,"HYPERLINK":playerHyperlinks, "DATE OF BIRTH":playerDatesOfBirth, "POSITION":playerPositions, "FOOT":playerFoots, "AGENT":playerAgents})
+	df.to_excel(path)
+	
+	sg.Popup("End of export")
--- test.py
+++ test.py
@@ -6,6 +6,10 @@ import codecs
 import json
 from multiprocessing import Pool
 from Scripts.Other.CountriesFromTransfermarkt import *
+from Scripts.Other.LeaguesFromCountries import getLeaguesFromCountry
+from Scripts.Team.TeamsFromLeague import findTeamsFromLeague
+from Scripts.Team.PlayersFromTeam import findPlayersFromTeam
+from Scripts.Player.AttributesFromPlayer import findPlayerAttributes
 
 COUNTRIES = []
 countriesJSON = getCountriesFromFile()
@@ -22,7 +26,28 @@ records = p.map(getLeaguesFromCountry, hyperlinks)
 p.terminate()
 p.join()
 
-for record in records:
-    print(record)
+
+leaguesHyperlinks = []
+
+for record in records[5]:
+    leaguesHyperlinks.append(record['hyperlink'])
+
+print(leaguesHyperlinks[0])
+
+teamIds, teamNames, teamHyperlinks = findTeamsFromLeague(leaguesHyperlinks[0])
+
+p = Pool(50)
+recordsPlayersFromTeams = p.map(findPlayersFromTeam, teamHyperlinks)
+p.terminate()
+p.join()
+
+print(recordsPlayersFromTeams[3][2])
 
 
+p = Pool(50)
+recordsAttributes = p.map(findPlayerAttributes, recordsPlayersFromTeams[3][2])
+p.terminate()
+p.join()
+
+print(recordsAttributes[0])
+# teamIds, teamNames, teamHyperlinks = findTeamsFromLeague(hyperlinks[6])
