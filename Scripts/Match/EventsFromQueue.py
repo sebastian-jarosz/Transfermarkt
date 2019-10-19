89afed04fa59d3a4151400fe54d48ed5853d2343
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-10-19 18:25:25 +0200

    Events working from GUI - Pool

--- Scripts/Match/EventsFromQueue.py
+++ Scripts/Match/EventsFromQueue.py
@@ -1,5 +1,5 @@
 from Scripts.Match.MatchesFromQueue import getMatchesFormQueue
-from Scripts.Match.EventsFromMatch import getEventsFromMatch
+from Scripts.Match.EventsFromMatch import getEventsFromMatchPool
 import time
 import pandas as pd
 from pathlib import Path
@@ -30,16 +30,14 @@ def generateEventsFromQueue(countryName, leagueName, saison, queueNumber, queueH
     tempTable5 = []
 
     for i in range(0, len(matchesHyperlinks)):
-        sg.OneLineProgressMeter('Export', i, len(matchesHyperlinks), 'key','Export of events from queue')
         print(matchesHyperlinks[i])
         time.sleep(1)
-        tempPlayersInMatchIds, tempPlayersInMatchTimes, tempGoalsIds, tempAssistsIds = getEventsFromMatch(matchesHyperlinks[i], saison)
+        tempPlayersInMatchIds, tempPlayersInMatchTimes, tempGoalsIds, tempAssistsIds = getEventsFromMatchPool(matchesHyperlinks[i], saison)
         tempTable1.extend(tempPlayersInMatchIds)
         tempTable2.extend(tempPlayersInMatchTimes) 
         tempTable3.append(tempGoalsIds) 
         tempTable4.append(tempAssistsIds)
 
-    sg.OneLineProgressMeter('Export',  len(matchesHyperlinks), len(matchesHyperlinks), 'key','Export of events from queue')
     df1 = pd.DataFrame(tempTable1)
     df1['TIMES'] = tempTable2
     df2 = pd.DataFrame(tempTable3)
