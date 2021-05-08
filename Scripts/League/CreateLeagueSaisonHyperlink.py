d8801ca71935b7d2d8076c5b81aee261271f8624
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-05-08 15:53:11 +0200

    remove unnecessary replace from generate_league_season_queue_hyperlink

--- Scripts/League/CreateLeagueSaisonHyperlink.py
+++ Scripts/League/CreateLeagueSaisonHyperlink.py
@@ -5,6 +5,5 @@ def generate_league_season_hyperlink(leagueHyperlink, year):
 
 def generate_league_season_queue_hyperlink(leagueHyperlink, year, queueNumber):
     league_season_hyperlink = generate_league_season_hyperlink(leagueHyperlink, year)
-    league_season_hyperlink = league_season_hyperlink.replace("startseite", "spieltag")
     league_season_queue_hyperlink = league_season_hyperlink + "&spieltag=" + str(queueNumber)
     return league_season_queue_hyperlink
